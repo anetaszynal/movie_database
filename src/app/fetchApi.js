a17acd43ac50a84b50b6b86777192d60f5f3f8c3
Aneta Szynal <anetaszynal28@gmail.com>
2020-11-28 13:09:43 +0100

    feat(): create functions to fetch from API with parameters

---
+++ src/app/fetchApi.js
@@ -0,0 +1,25 @@
+import { buildQueryString } from "./buildQueryString";
+
+const API_KEY = "fa9cbe4630e747b288e3a22b5a8069ea";
+const API_URL = "https://api.themoviedb.org/3";
+
+export const fetchApi = async ({path, parameters}) => {
+  const defaultParameter = {
+    api_key: API_KEY,
+  };
+
+  const allParameters = {
+    ...defaultParameter,
+    ...(
+        parameters || {}
+    )
+  };
+
+  const response = await fetch(`${API_URL}${path}?${buildQueryString(allParameters)}`);
+
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+
+  return response.json();
+};
