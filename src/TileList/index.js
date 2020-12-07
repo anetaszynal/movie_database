07015fdc01f6c901fe4fafbd18b47f5d780d4c0c
Aneta Szynal <anetaszynal28@gmail.com>
2020-12-07 21:09:43 +0100

    feat(): add title and container

--- src/TileList/index.js
+++ src/TileList/index.js
@@ -1,14 +1,28 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const TileList = styled.section`
-  //padding: 16px;
+export const Container = styled.main`
+  padding: 16px;
   max-width: 1368px;
   margin: 0 auto;
+`
+
+export const TileList = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
   grid-gap: 24px;
   
-  @media(max-width: ${({theme}) => theme.breakpoint.mobile}) {
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
       grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
     }
-`;
+`
+
+export const Title = styled.h1`
+  font-size: 36px;
+  line-height: 1.2;
+  margin: 56px 0 24px;
+  
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+      font-size: 18px;
+       margin: 24px 0 12px;
+    }
+`
--- src/app/Movies/MoviesPage.js
+++ src/app/Movies/MoviesPage.js
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { actions, selectors } from './topRatedMoviesSlice'
 import { selectGenres } from '../genre/genreSlice'
 import { Tile } from '../../Tile'
-import { TileList } from '../../TileList'
+import { TileList, Title, Container } from '../../TileList'
 import { getYear } from '../../lib/utils'
 
 export const MoviesPage = () => {
   const dispatch = useDispatch()
   const movies = useSelector(selectors.selectResults)
   const status = useSelector(selectors.selectStatus)
-  const genres = useSelector(selectGenres);
+  const genres = useSelector(selectGenres)
 
   useEffect(() => {
     dispatch(actions.fetch({ page: 1 }))
@@ -19,17 +19,20 @@ export const MoviesPage = () => {
 
   return (
     status === 'success' && (
-      <TileList>
-        {movies?.map(movie => (
-          <Tile key = {movie.id}
-                url = {movie.poster_path}
-                genres={movie.genre_ids.map(id => genres[id])}
-                title = {movie.title}
-                caption = {getYear(movie.release_date)}
-                averageVotes = {movie.vote_average}
-                votes = {movie.vote_count}/>
-        ))}
-      </TileList>
+      <Container>
+        <Title>Top rated Movies</Title>
+        <TileList>
+          {movies?.map(movie => (
+            <Tile key = {movie.id}
+                  url = {movie.poster_path}
+                  genres = {movie.genre_ids.map(id => genres[id])}
+                  title = {movie.title}
+                  caption = {getYear(movie.release_date)}
+                  averageVotes = {movie.vote_average}
+                  votes = {movie.vote_count}/>
+          ))}
+        </TileList>
+      </Container>
     )
   )
 }
