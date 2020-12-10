69f5f328bf47e13a6fa567a97770cc0b56b4126f
Aneta Szynal <anetaszynal28@gmail.com>
2020-12-10 15:04:10 +0100

    refactor(): add path to object

--- src/app/Movies/getTopRatedMovies.js
+++ src/app/Movies/getTopRatedMovies.js
@@ -1,14 +1,15 @@
 import { fetchApi } from '../fetchApi'
+import { APP_ROUTES } from '../../lib/utils'
 
 export const getTopRatedMovies = ({ page }) => fetchApi({
-  path: '/movie/top_rated',
+  path: APP_ROUTES.movies,
   parameters: {
     page,
   }
 })
 
 export const getSearchedMovies = ({ page, query }) => fetchApi({
-  path: '/search/movie',
+  path: APP_ROUTES.searchMovies,
   parameters: {
     page,
     query,
