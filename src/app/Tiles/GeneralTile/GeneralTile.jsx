b41315c2cdac6c118e5a6e1317d51a7294380fcd
Aneta Szynal <anetaszynal28@gmail.com>
2020-12-14 15:38:01 +0100

    refactor():pass responsibility to parent component

--- src/app/Tiles/GeneralTile/GeneralTile.jsx
+++ src/app/Tiles/GeneralTile/GeneralTile.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Caption, InsideMeta, TileWrapper, Title } from './styled'
-import { Image, Meta } from '../BothTileStyle'
-import { Vote } from '../Vote'
-import { GenresList } from '../GenresList'
+import React from "react";
+import { Caption, InsideMeta, TileWrapper, Title } from "./styled";
+import { Image, Meta } from "../BothTileStyle";
+import { Vote } from "../Vote";
+import { GenresList } from "../GenresList";
 
-export const GeneralTile = ({ url, title, genres, caption, people, averageVotes, votes }) => (
-  <TileWrapper people = {people}>
-    <Image url = {url}/>
+export const GeneralTile = ({
+  url,
+  title,
+  genres,
+  caption,
+  people,
+  averageVotes,
+  votes,
+}) => (
+  <TileWrapper people={people}>
+    <Image url={url} />
     <Meta people={people}>
       <InsideMeta>
-        <Title people = {people}>{title}</Title>
-        {!!caption && (
-          <Caption>{caption}</Caption>
-        )}
-        <GenresList genres = {genres}/>
+        <Title people={people}>{title}</Title>
+        {!!caption && <Caption>{caption}</Caption>}
+        {genres?.length > 0 && <GenresList genres={genres} />}
       </InsideMeta>
-      <Vote votes = {votes} averageVotes = {averageVotes} people = {people}/>
+      {!people && <Vote votes={votes} averageVotes={averageVotes} />}
     </Meta>
   </TileWrapper>
-)
+);
--- src/app/Tiles/GenresList/GenresList.jsx
+++ src/app/Tiles/GenresList/GenresList.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import { List, ListItem } from './styled'
+import React from "react";
+import { List, ListItem } from "./styled";
 
 export const GenresList = ({ genres }) => (
-  genres?.length > 0 && (
-    <List>
-      {genres?.map((genre, index) => (
-        <ListItem key = {index}>{genre}</ListItem>
-      ))}
-    </List>
-  )
-)
+  <List>
+    {genres?.map((genre, index) => (
+      <ListItem key={index}>{genre}</ListItem>
+    ))}
+  </List>
+);
