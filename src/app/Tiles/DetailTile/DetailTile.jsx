9499a58bdec3f3e6078b5edb727915d130e00485
Aneta Szynal <anetaszynal28@gmail.com>
2020-12-14 15:35:54 +0100

    refactor(): add boolean value to styles attribute

--- src/app/Tiles/DetailTile/DetailTile.jsx
+++ src/app/Tiles/DetailTile/DetailTile.jsx
@@ -1,22 +1,49 @@
-import { GenresList } from '../GenresList'
-import { Vote } from '../Vote'
-import React from 'react'
-import { AdditionalInfo, Caption, DetailTileWrapper, Name, Title, Description } from './styled'
-import { Image, Meta } from '../BothTileStyle'
+import { GenresList } from "../GenresList";
+import { Vote } from "../Vote";
+import React from "react";
+import {
+  AdditionalInfo,
+  Caption,
+  DetailTileWrapper,
+  Name,
+  Title,
+  Description,
+} from "./styled";
+import { Image, Meta } from "../BothTileStyle";
 
-export const DetailTile = ({ url, title, genres, caption, averageVotes, votes, production, date, description, detail }) => (
+export const DetailTile = ({
+  url,
+  title,
+  genres,
+  caption,
+  averageVotes,
+  votes,
+  production,
+  date,
+  description,
+  detail,
+  people,
+}) => (
   <DetailTileWrapper>
-    <Image url = {url}/>
-    <Meta detail = {detail}>
+    <Image url={url} />
+    <Meta detail={detail}>
       <Title>{title}</Title>
       <Caption>{caption}</Caption>
       <div>
-        <AdditionalInfo><Name>Production:</Name>{production}</AdditionalInfo>
-        <AdditionalInfo last = "true"><Name>Release date:</Name>{date}</AdditionalInfo>
+        <AdditionalInfo>
+          <Name>Production:</Name>
+          {production}
+        </AdditionalInfo>
+        <AdditionalInfo last={true}>
+          <Name>Release date:</Name>
+          {date}
+        </AdditionalInfo>
       </div>
-      <GenresList genres = {genres}/>
-      <Vote averageVotes = {averageVotes} votes = {votes} detail={detail}/>
+      {genres?.length > 0 && <GenresList genres={genres} />}
+      {!people && (
+        <Vote averageVotes={averageVotes} votes={votes} detail={detail} />
+      )}
     </Meta>
     <Description>{description}</Description>
   </DetailTileWrapper>
-)
+);
