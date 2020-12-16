f9cfce42bb51fbbf185fd95d6c594b4fa0382636
Aneta Szynal <anetaszynal28@gmail.com>
2020-12-16 22:20:21 +0100

    feat(): add Loading page

---
+++ src/common/Page/Content/Loading/index.js
@@ -0,0 +1,20 @@
+import styled, { keyframes } from 'styled-components'
+import { ReactComponent as LoaderIcon } from './LoadingIcon.svg'
+
+const rotate = keyframes`
+  to{
+    transform: rotate(360deg);
+  }
+`
+
+export const Loader = styled(LoaderIcon)`
+  animation: ${rotate} 1s linear infinite;
+  margin: 120px auto 0;
+  display: block;
+  height: auto;
+  
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}){
+    margin-top: 24px;
+    width: 35px;
+  }
+`
