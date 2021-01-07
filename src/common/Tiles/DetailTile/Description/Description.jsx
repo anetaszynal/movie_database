40e2a80c3df879b345efd7c9a0ab7eecdade6fad
Aneta Szynal <anetaszynal28@gmail.com>
2021-01-07 18:26:57 +0100

    feat(): add change words number in description by diffrent screen width

--- src/common/Tiles/DetailTile/Description/Description.jsx
+++ src/common/Tiles/DetailTile/Description/Description.jsx
@@ -1,15 +1,25 @@
-import React, { useMemo, useState } from 'react'
-import {ButtonTextVisibility} from './styled'
+import React, { useEffect, useMemo, useState } from 'react'
+import { ButtonTextVisibility } from './styled'
 
 export const Description = ({ text }) => {
   const isTextLongOne = useMemo(() => text?.length > 700, [text])
   const [cutText, setCutText] = useState(isTextLongOne)
 
+  const howMuchWords = () => {
+    const screenWidth = window.innerWidth
+    if(screenWidth > 787){
+     return Math.floor((Math.min(screenWidth, 1400) - (16 * 2 + 64 * 3 + 310)) / 6)
+    }
+    else {
+      return Math.floor((screenWidth - 64) / 10)
+    }
+  }
+
   const renderTruncatedText = () => (
     <>
-      {text.split(' ').slice(0, 100).join(' ')}
+      {text.split(' ').slice(0, howMuchWords()).join(' ')}
       <ButtonTextVisibility onClick = {() => setCutText(false)}>{'...read more'}</ButtonTextVisibility>
-      </>
+    </>
   )
 
   const renderFullText = () => (
